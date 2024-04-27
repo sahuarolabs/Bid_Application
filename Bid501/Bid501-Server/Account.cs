6de5e8ecf8987b93b1fee7e642c93b3da036de2e
Bobadylan <122490929+Bobadylan@users.noreply.github.com>
2024-04-27 18:12:22 -0500

    Adding some files

---
+++ Bid501/Bid501-Server/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bid501_Server
+{
+    public class Account
+    {
+        public int ID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool LoginStatus { get; set; }       
+
+        public AccountType Type { get; private set; }
+
+        public Account(AccountType type)
+        {
+            Type = type;
+        }
+
+        public AccountType ValidateAccountType(string username, string password)
+        {
+            if(username == UserName && password == Password)
+            {
+                return Type;
+            }
+        }
+    }
+}
---
+++ Bid501/Bid501-Server/AllAccounts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bid501_Server
+{
+    public class AllAccounts
+    {
+        public List<Account> AccountList { get; set; }
+    }
+}
